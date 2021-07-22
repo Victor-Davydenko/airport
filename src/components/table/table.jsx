f0f855ab5b5fcd3e33ed054919d40a92e9af114d
Victor-Davydenko <59482820+Victor-Davydenko@users.noreply.github.com>
2021-07-22 19:54:51 +0300

    table done

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -14,8 +14,39 @@ const Table = () => {
             <th>Статус</th>
             <th>Авіакомпанія</th>
             <th>Рейс</th>
+            <th></th>
           </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td>А</td>
+            <td>2:00</td>
+            <td>Анталія</td>
+            <td>Вилетів о 2:13</td>
+            <td>
+              <img className='image' src="https://api.iev.aero/media/airline/files/5b556bea9e8d9364778468.png" alt=""/>
+              <span className='company'>WizzAir</span>
+            </td>
+            <td>7W9235</td>
+            <td>
+              <a href="" className="table__link">Деталі рейсу</a>
+            </td>
+          </tr>
+          <tr>
+            <td>А</td>
+            <td>2:00</td>
+            <td>Анталія</td>
+            <td>Вилетів о 2:13</td>
+            <td>
+              <span className="image"><img src="" alt=""/></span>
+              WizzAir
+            </td>
+            <td>7W9235</td>
+            <td>
+              <a href="" className="table__link">Деталі рейсу</a>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
