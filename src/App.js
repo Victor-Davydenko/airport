475751a0d736651ba4e803915a1c232639845f5c
Victor-Davydenko <59482820+Victor-Davydenko@users.noreply.github.com>
2021-07-22 14:21:10 +0300

    searchField done

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import React from 'react';
 import './App.scss';
 
 import Header from './components/header';
+import SearchField from "./components/search";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <SearchField />
+      </div>
     </div>
   );
 }
