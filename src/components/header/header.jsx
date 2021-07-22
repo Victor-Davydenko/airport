d8c931f5b5a5d261708674a6450aa18db56d3555
Victor-Davydenko <59482820+Victor-Davydenko@users.noreply.github.com>
2021-07-22 13:13:59 +0300

    header done

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 
 import './header.scss';
+import logo from '../../images/logo@2x.webp'
 
 const Header = () => {
   return (
     <header className="header">
-      HH
+      <div className="header__container">
+        <div className="header__inner">
+          <a href="/" className="logo">
+            <img src={logo} alt="logo"/>
+          </a>
+          <nav className="navigation">
+            <ul className="navigation__list">
+              <li className="navigation__list-item">
+                <a href="#" className="navigation__link">Пасажирам</a>
+              </li>
+              <li className="navigation__list-item">
+                <a href="#" className="navigation__link">Послуги IEV</a>
+              </li>
+              <li className="navigation__list-item">
+                <a href="#" className="navigation__link">VIP</a>
+              </li>
+              <li className="navigation__list-item">
+                <a href="#" className="navigation__link">Партнерам</a>
+              </li>
+              <li className="navigation__list-item">
+                <a href="#" className="navigation__link">Прес-центр</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </header>
   )
 };
