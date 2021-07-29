1c85da985b4746331e828acc6848cfc3b899a1bb
Victor-Davydenko <59482820+Victor-Davydenko@users.noreply.github.com>
2021-07-29 12:07:40 +0300

    filter behavior done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
 import './App.scss';
 
 import Header from './components/header';
@@ -9,6 +9,25 @@ import Table from './components/table';
 import Footer from './components/footer';
 
 function App() {
+	const [state, setState] = useState({
+		flightDirection: 'arrivals',
+		chosenDate: new Date(),
+		activeDate: new Date().toLocaleDateString().slice(0, 10).replace(/\./g, '-'),
+		isYesterdayPicked: false,
+		isTodayPicked: true,
+		isTomorrowPicked: false,
+	});
+
+	const {
+		flightDirection,
+		chosenDate,
+		isYesterdayPicked,
+		isTodayPicked,
+		isTomorrowPicked,
+	} = state;
+	const today = moment(new Date())._d;
+	const tomorrow = moment(new Date()).add(1, 'days')._d;
+	const yesterday = moment(new Date()).add(-1, 'days')._d;
 	useEffect(() => {
 		const body = document.querySelector('body');
 		body.addEventListener('mousemove', () => {
@@ -23,13 +42,73 @@ function App() {
 			}
 		});
 	}, []);
+	const chooseFlightDirection = (flightDirection) => {
+		setState({
+			...state,
+			flightDirection,
+		});
+	};
+	const chooseDate = (date) => {
+		setState({
+			...state,
+			chosenDate: date,
+			isTodayPicked: date === new Date(),
+			isTomorrowPicked: false,
+			isYesterdayPicked: false,
+		});
+	};
+	const pickDate = (pickedDay) => {
+		if (pickedDay === 'yesterday') {
+			setState({
+				...state,
+				chosenDate: yesterday,
+				isTodayPicked: false,
+				isTomorrowPicked: false,
+				isYesterdayPicked: true,
+			});
+		}
+		if (pickedDay === 'today') {
+			setState({
+				...state,
+				chosenDate: today,
+				isTodayPicked: true,
+				isTomorrowPicked: false,
+				isYesterdayPicked: false,
+			});
+		}
+		if (pickedDay === 'tomorrow') {
+			setState({
+				...state,
+				chosenDate: tomorrow,
+				isTodayPicked: false,
+				isTomorrowPicked: true,
+				isYesterdayPicked: false,
+			});
+		}
+	};
+	const activeDate = chosenDate.toLocaleDateString().slice(0, 10).replace(/\./g, '-');
+	const filterProps = {
+		chooseFlightDirection,
+		chooseDate,
+		pickDate,
+		flightDirection,
+		chosenDate,
+		today,
+		tomorrow,
+		yesterday,
+		isTodayPicked,
+		isTomorrowPicked,
+		isYesterdayPicked,
+	};
 	return (
 		<div className="App">
 			<Header />
 			<div className="container">
 				<SearchField />
-				<Filter />
-				<Table />
+				<Filter {...filterProps}/>
+				<Table activeDate={activeDate}
+							 flightDirection={flightDirection}
+				/>
 			</div>
 			<Footer />
 		</div>
--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import DatePicker from 'react-datepicker';
 
 import 'react-datepicker/dist/react-datepicker.css';
@@ -6,40 +7,64 @@ import 'react-datepicker/dist/react-datepicker.css';
 import './filter.scss';
 import icon from '../../images/dateIcon.png';
 
-const Filter = () => (
-	<div className='filter'>
-		<div className="filter__row">
-			<ul className="filter__flights-list">
-				<li className="filter__flights-list-item active">
-					<span>Виліт</span>
-				</li>
-				<li className="filter__flights-list-item">
-					<span>Приліт</span>
-				</li>
-			</ul>
-		</div>
-		<div className="filter__row filter__date-row">
-			<div className="calendar">
-				<DatePicker />
-				<img src={icon} alt="date-picker"/>
-				<span className='calendar__date'>22/07</span>
+const Filter = ({
+	flightDirection,
+	chosenDate,
+	isYesterdayPicked,
+	isTodayPicked,
+	isTomorrowPicked,
+	today,
+	tomorrow,
+	yesterday,
+	chooseFlightDirection,
+	chooseDate,
+	pickDate,
+}) => {
+	const arrivalsClassNames = `filter__flights-list-item ${flightDirection === 'arrivals' ? 'filter__flights-list-item--active' : ''}`;
+	const departureClassNames = `filter__flights-list-item ${flightDirection === 'departure' ? 'filter__flights-list-item--active' : ''}`;
+	const yesterdayClassNames = `filter__date-list-item ${isYesterdayPicked ? 'filter__date-list-item--active' : ''}`;
+	const todayClassNames = `filter__date-list-item ${isTodayPicked ? 'filter__date-list-item--active' : ''}`;
+	const tomorrowClassNames = `filter__date-list-item ${isTomorrowPicked ? 'filter__date-list-item--active' : ''}`;
+	const formatDateToString = (date) => {
+		const day = date.toLocaleString().slice(0, 2);
+		const month = date.toLocaleString().slice(3, 5);
+		return `${day}/${month}`;
+	};
+	return (
+		<div className='filter'>
+			<div className="filter__row">
+				<ul className="filter__flights-list">
+					<li className={ departureClassNames } onClick={() => chooseFlightDirection('departure')}>
+						<span>Виліт</span>
+					</li>
+					<li className={ arrivalsClassNames } onClick={() => chooseFlightDirection('arrivals')}>
+						<span>Приліт</span>
+					</li>
+				</ul>
 			</div>
-			<ul className="filter__date-list">
-				<li className="filter__date-list-item">
-					<span className="date">21/07</span>
-					<span className="text">Вчора</span>
-				</li>
-				<li className="filter__date-list-item active">
-					<span className="date">22/07</span>
-					<span className="text">Сьогодні</span>
-				</li>
-				<li className="filter__date-list-item">
-					<span className="date">23/07</span>
-					<span className="text">Завтра</span>
-				</li>
-			</ul>
+			<div className="filter__row filter__date-row">
+				<div className="calendar">
+					<DatePicker selected={ chosenDate } onChange={(date) => chooseDate(date)}/>
+					<img src={icon} alt="date-picker"/>
+					 <span className='calendar__date'>{formatDateToString(chosenDate)}</span>
+				</div>
+				 <ul className="filter__date-list">
+					<li className={yesterdayClassNames} onClick={() => pickDate('yesterday')}>
+						<span className="date">{formatDateToString(yesterday)}</span>
+						<span className="text">Вчора</span>
+					</li>
+					<li className={todayClassNames} onClick={() => pickDate('today')}>
+						<span className="date">{formatDateToString(today)}</span>
+						<span className="text">Сьогодні</span>
+					</li>
+					<li className={tomorrowClassNames} onClick={() => pickDate('tomorrow')}>
+						<span className="date">{formatDateToString(tomorrow)}</span>
+						<span className="text">Завтра</span>
+					</li>
+				 </ul>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Filter;
